29f1b0de9db8f0db8a6e13f2e2fd3306b934b5a2
tonnyblair62 <tonnyblairma@gmail.com>
2021-07-24 12:50:01 +0300

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,47 @@
-// Javascript
-var customerMessage = [];
+$(document).ready(function () {
+    $(".design-img").click(function () {
+      $(".design-text").toggle();
+      $(".design-img").hide();
+    });
+    $(".development-img").click(function () {
+      $(".development-text").toggle();
+      $(".development-img").hide();
+    });
+    $(".product-img").click(function () {
+      $(".product-text").toggle();
+      $(".product-img").hide();
+    });
+    $(".design-text").click(function () {
+      $(".design-img").toggle();
+      $(".design-text").hide();
+    });
+    $(".development-text").click(function () {
+      $(".development-img").toggle();
+      $(".development-text").hide();
+    });
+    $(".product-text").click(function () {
+      $(".product-img").toggle();
+      $(".product-text").hide();
+    });
+  });
 
-function showCustomerMessage() {
+  $(document).ready(function () {
+    $(".col-md-3").mouseover(function () {
+      $(".col-md-3").css("border-size", "5px");
+    });
+  });
+
+
+  function myname() {
     var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("textarea").value;
-
-    if (name && email && message) {
-        customerMessage.push(name, email, message);
-        return name;
-    }
-}
-
-// jQuery
-$(document).ready(function() {
-
-    //Preloader
-    $(window).on("load", function() {
-        preloaderFadeOutTime = 3000;
-
-        function hidePreloader() {
-            var preloader = $('.preloader');
-            preloader.fadeOut(preloaderFadeOutTime);
-        }
-        hidePreloader();
-    });
-
-    // Design: Hide image and show paragraph
-    $(".design-initially-showing").click(function() {
-        $(".design-initially-hidden").slideToggle();
-        $(".design-initially-showing").slideToggle();
-    });
-
-    // Design: Hide paragraph and show image
-    $(".design-initially-hidden").click(function() {
-        $(".design-initially-hidden").slideToggle();
-        $(".design-initially-showing").slideToggle();
-    });
-
-    // Development: Hide image and show paragraph
-    $(".dev-initially-showing").click(function() {
-        $(".dev-initially-hidden").slideToggle();
-        $(".dev-initially-showing").slideToggle();
-    });
-
-    // Development: Hide paragraph and show image
-    $(".dev-initially-hidden").click(function() {
-        $(".dev-initially-hidden").slideToggle();
-        $(".dev-initially-showing").slideToggle();
-    });
-
-    // Product Management: Hide image and show paragraph
-    $(".p-mgt-initially-showing").click(function() {
-        $(".p-mgt-initially-hidden").slideToggle();
-        $(".p-mgt-initially-showing").slideToggle();
-    });
-
-    // Product Management: Hide paragraph and show image
-    $(".p-mgt-initially-hidden").click(function() {
-        $(".p-mgt-initially-hidden").slideToggle();
-        $(".p-mgt-initially-showing").slideToggle();
-    });
-
-    // Hover effect on portfolio images
-    $(".portfolio img").hover(function() {
-        $(".col-md-3 img").addClass("image");
-        $(".project-name").show();
-    });
-
-    $("#button").click(function() {
-
-        event.preventDefault();
-    });
+    if (name === "" || email == "") {
+      alert("Please enter name and email correctly...");
+    } else {
+      alert(name + "" + " we have recieved your message");
+    };
+  };
 
 
-
-    $("#button").click(function() {
-        if (showCustomerMessage()) {
-            $("#message-alert").text("Hi " + showCustomerMessage() + ", \nThank you for reaching out to us, we will respond as soon as possible.");
-            $("#message-alert").addClass("alert-success");
-            $("#message-alert").removeClass("alert-danger");
-            $("#message-alert").show();
-        } else {
-            $("#message-alert").text("Please fill in all fields");
-            $("#message-alert").removeClass("alert-success");
-            $("#message-alert").addClass("alert-danger");
-            $("#message-alert").show();
-        }
-
-
-    });
-
-    $(button).on("click", function() {
-        alertFadeOutTime = 5000;
-
-        function hideAlert() {
-            var messageAlert = $('#message-alert');
-            messageAlert.fadeOut(alertFadeOutTime);
-        }
-        hideAlert();
-    });
-});
+  function reset() {
+    document.getElementById("myform").reset();
+  }
